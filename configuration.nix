8ff819da9d9a3d8ab6c69207676545e3d22fd91d
Joshua <59619378+Joshua154@users.noreply.github.com>
2025-05-14 23:24:11 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -4,10 +4,14 @@
 
 { config, pkgs, lib, ... }:
 
+#let
+#  home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz;
+#in
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      #(import "${home-manager}/nixos")
     ];
 
   # Bootloader.
@@ -15,10 +19,10 @@
   boot.plymouth.theme = "breeze"; #bgrt
   boot.kernelParams = [ "quiet" "splash" ];
 
-  virtualisation.virtualbox.host.enable = true;
+  #virtualisation.virtualbox.host.enable = true;
 
   #boot.binfmt.emulatedSystems = [ "i686-linux" ];
-    
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
@@ -90,7 +94,7 @@
   users.users.joshua = {
     isNormalUser = true;
     description = "Joshua";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
     packages = with pkgs; [
     #  thunderbird
     ];
@@ -98,10 +102,20 @@
     shell = pkgs.zsh;
   };
 
+  #home-manager.users.joshua = { pkgs, ... }: {
+  #  home.packages = [ pkgs.atool pkgs.httpie ];
+  #  programs.bash.enable = true;
+ #
+ #   # The state version is required and should stay at the version you
+ #   # originally installed.
+ #   home.stateVersion = "24.11";
+ # };
+
   fonts.packages = with pkgs; [
     (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
   ];
 
+  virtualisation.docker.enable = true;
 
   nix.gc = {
     automatic = true;
@@ -137,13 +151,16 @@
     zoxide
     ghostty
     gromit-mpx
-    
+    direnv
+
     obsidian
-    vesktop
+    #vesktop
+    discord
     termius
     jetbrains-toolbox
 
     jdk21_headless
+    python312
 
     steam
     spotify
@@ -204,4 +221,43 @@
   in [
     nightlyOverlay
   ];
+
+  # Enable OpenGL
+  hardware.graphics = {
+    enable = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+
+    open = false;
+
+    # Enable the Nvidia settings menu,
+        # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 }

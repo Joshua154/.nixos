aea4f7a1d1c785921d92d38b7d3012bea465d223
Joshua <59619378+Joshua154@users.noreply.github.com>
2025-05-21 20:58:11 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,11 @@
       #(import "${home-manager}/nixos")
     ];
 
+  programs.kdeconnect = {
+    enable = true;
+    package = pkgs.gnomeExtensions.gsconnect;
+  };
+
   # Bootloader.
   boot.plymouth.enable = true;
   boot.plymouth.theme = "breeze"; #bgrt
@@ -65,6 +70,7 @@
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
+
     variant = "";
   };
 
@@ -158,6 +164,8 @@
     discord
     termius
     jetbrains-toolbox
+    kdePackages.kdeconnect-kde
+    gnomeExtensions.gsconnect
 
     jdk21_headless
     python312
@@ -165,8 +173,8 @@
     steam
     spotify
     modrinth-app
-    #legendary-gl
-    #wineWowPackages.stable
+    legendary-gl
+    wineWowPackages.stable
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -239,7 +247,7 @@
     # Enable this if you have graphical corruption issues or application crashes after waking
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
     # of just the bare essentials.
-    powerManagement.enable = false;
+    powerManagement.enable = true;
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
@@ -258,6 +266,6 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    #package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 }

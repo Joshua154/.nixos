e4d16fb4135b315160e712a2bf3e5cebb0400992
Joshua Neunert <joshua.neunert@gmail.com>
2025-06-07 23:37:39 +0200

    feat: added home Manager

--- flake.nix
+++ flake.nix
@@ -4,9 +4,12 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, home-manager, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -20,6 +23,13 @@
         modules = [
           ./configuration.nix
         ];
+      };
+
+      homeConfigurations = {
+	joshua = home-manager.lib.homeManagerConfiguration {
+	  inherit pkgs;
+	  modules = [ ./home.nix ];
+	};	
       };
     };
 }

e571412bc43ee10a187b2268c678788741b40121
Joshua Neunert <joshua.neunert@gmail.com>
2025-08-20 19:29:37 +0200

    feat: integrate omarchy-nix

--- flake.nix
+++ flake.nix
@@ -9,9 +9,15 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     hyprland.url = "github:hyprwm/Hyprland";
+    
+    omarchy-nix = {
+      url = "github:henrysipp/omarchy-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, flake-utils, omarchy-nix, home-manager, ... }@inputs:
     let
       system = "x86_64-linux";
       hostname = "JNix";
@@ -30,14 +36,22 @@
         modules = [
           ./hosts/JNix
 
+          omarchy-nix.nixosModules.default
           home-manager.nixosModules.home-manager
           {
+            omarchy = {
+              full_name = "Your Name";
+              email_address = "your.email@example.com";
+              theme = "tokyo-night";
+            };
+          
             home-manager = {
               useGlobalPkgs = true;
               useUserPackages = true;
               users."${username}" = {
                 imports = [
                   ./home
+                  omarchy-nix.homeManagerModules.default
                 ];
               };
             };

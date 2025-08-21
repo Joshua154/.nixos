cf3354efd303c8233c1eb8ea2cfcc4d0cc320fb5
Joshua Neunert <joshua.neunert@gmail.com>
2025-08-21 16:35:07 +0200

    clean: comment out xdg config

--- home/programs/viewer/gthumb.nix
+++ home/programs/viewer/gthumb.nix
@@ -4,14 +4,14 @@
     gthumb
   ];
 
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = {
-      "image/jpeg"  = [ "gthumb.desktop" ];
-      "image/png"   = [ "gthumb.desktop" ];
-      "image/webp"  = [ "gthumb.desktop" ];
-      "image/tiff"  = [ "gthumb.desktop" ];
-      "image/heif"  = [ "gthumb.desktop" ];
-    };
-  };
+#  xdg.mimeApps = {
+#    enable = true;
+#    defaultApplications = {
+#      "image/jpeg"  = [ "gthumb.desktop" ];
+#      "image/png"   = [ "gthumb.desktop" ];
+#      "image/webp"  = [ "gthumb.desktop" ];
+#      "image/tiff"  = [ "gthumb.desktop" ];
+#      "image/heif"  = [ "gthumb.desktop" ];
+#    };
+#  };
 }

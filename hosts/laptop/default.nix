2ce3c8f2a62b2e7f4cd2a428505e6e68a4d20c33
Joshua Neunert <joshua.neunert@gmail.com>
2026-03-06 21:41:06 +0100

    feat: add initial configuration files for laptop host

---
+++ hosts/laptop/default.nix
@@ -0,0 +1,34 @@
+# Example configuration for a second host (laptop)
+{ config, hostname, inputs, pkgs, system, username, ... }:
+
+{
+  imports = [
+    ../../modules/system/shared.nix
+
+    ./hardware-configuration.nix
+
+    ../../modules/profiles/hardware/intel.nix
+
+    # Display Manager
+    # ../../modules/profiles/display-managers/ly.nix
+    ../../modules/profiles/display-managers/gdm.nix
+    # ../../modules/profiles/display-managers/sddm.nix
+
+    # Window Manager
+    # ../../modules/profiles/window-managers/hyprland.nix
+    # ../../modules/profiles/window-managers/bspwm.nix
+    ../../modules/profiles/window-managers/gnome.nix
+  ];
+
+  networking.hostName = hostname;
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  services.tlp.enable = true;
+  powerManagement.enable = true;
+
+  system.stateVersion = "25.05";
+}

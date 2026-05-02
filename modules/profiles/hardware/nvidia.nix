640132b5965a23c76ad8b1b4946dd8bc2b29eb0a
Joshua Neunert <joshua.neunert@gmail.com>
2026-05-03 01:16:29 +0200

    feat: add VA-API for NVIDIA graphics

--- modules/profiles/hardware/nvidia.nix
+++ modules/profiles/hardware/nvidia.nix
@@ -5,6 +5,10 @@
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
+    extraPackages = with pkgs; [
+      nvidia-vaapi-driver
+      libvdpau-va-gl
+    ];
   };
 
   # nvidia driver for Xorg and Wayland

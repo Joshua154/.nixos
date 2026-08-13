a86e240cfbb0d41eda8854e417e2d848c0d3b63e
Joshua Neunert <joshua.neunert@gmail.com>
2026-08-13 14:43:09 +0200

    feat: made systemboot remember last booted option

--- modules/profiles/boot/systemd-boot.nix
+++ modules/profiles/boot/systemd-boot.nix
@@ -3,6 +3,10 @@
     systemd-boot = {
       enable = lib.mkDefault true;
       configurationLimit = lib.mkDefault 10;
+
+      extraInstallCommands = ''
+        sed -i 's/^default .*/default @saved/' /boot/loader/loader.conf
+      '';
     };
     efi.canTouchEfiVariables = lib.mkDefault true;
   };

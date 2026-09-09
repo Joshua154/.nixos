efc510ce511d758db004e1856d7bccde2019af75
Joshua Neunert <joshua.neunert@gmail.com>
2026-09-09 20:57:27 +0200

    feat: disabled vrr

--- home/windowManager/hyprland/hyprland.nix
+++ home/windowManager/hyprland/hyprland.nix
@@ -182,8 +182,7 @@ in {
         disable_hyprland_logo = true;
         disable_splash_rendering = true;
         mouse_move_enables_dpms = true;
-        # Restrict VRR to fullscreen applications to prevent idle desktop flicker.
-        vrr = 2;
+        vrr = false;
       };
       xwayland.force_zero_scaling = true;
       cursor.no_hardware_cursors = true;
